5e7b0a3e88188618b89b4ad0ee0d2bbb4d111cda
jones4wm <jones4wm@dukes.jmu.edu>
2025-04-22 15:10:44 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var config = {
-    style: 'mapbox://styles/burgerjh/cm98v4e0y001c01qn7swuc8m0',
+    style: 'https://api.mapbox.com/styles/v1/jones4wm/cm98v7cp600pw01quez9059qg.html?title=copy&access_token=pk.eyJ1Ijoiam9uZXM0d20iLCJhIjoiY205OHV3ZTUzMDRhZzJqcTFhcncxNW5reCJ9.4BS0zS0uyQ3WcX0yaMM0eg&zoomwheel=true&fresh=true#2/38/-34',
     // leave commented to use Mapbox Standard Style
     accessToken: 'pk.eyJ1IjoiYnVyZ2VyamgiLCJhIjoiY20yc3IyZXozMDBqdTJ2b3BpY3M3aWNqcCJ9.BRJ_B6H5_MYZ7Q_PBxcaQg',
     showMarkers: false,
@@ -15,9 +15,9 @@ var config = {
     theme: 'light',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'Color Purple Essay',
-    subtitle: 'Spatial Journeys across the South',
-    byline: 'My Name',
+    title: 'Mapping the Color Purple',
+    subtitle: 'Significant places mentioned by Alice Walker in her book',
+    byline: 'Will Jones',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
@@ -26,7 +26,7 @@ var config = {
             hidden: false,
             title: 'The Color Purple',
             image: './assets/color_purple_cover.jpg',
-            description: 'This is where I insert a description of the novel',
+            description: 'An epistolary novel that tells the story of Celie, a black woman who lived in rural isolation in Georgia and her journey of self discovery',
             location: {
                 center: [-90.3150946, 38.6605671],
                 zoom: 4,
@@ -56,7 +56,7 @@ var config = {
             hidden: false,
             title: 'Someplace Georgia',
             image: './assets/alice_walker_childhood_home.jpg',
-            description: 'Description of the second location.',
+            description: 'This was the place in Georgia where Celie lived her whole life, in relative isolation and ignorance',
             location: {
                 center: [-83.5573651, 32.5573651], 
                 zoom: 6.5,
@@ -81,7 +81,7 @@ var config = {
             hidden: false,
             title: 'Olinka',
             image: './assets/firestone_farm.jpg',
-            description: 'Description of the Olinka people',
+            description: 'The Olinka people are a fictional African tribe in the Color Purple who experience colonial oppression at the hands of the Englishmen running a rubber factory. Walker uses them to deepen her theme of oppression and cultural identity',
             location: {
                 center: [-10.378889436404226, 6.251275915595865], 
                 zoom: 8.00,
@@ -100,7 +100,7 @@ var config = {
             hidden: false,
             title: 'End',
             image: './assets/color_purple_field.jpg',
-            description: 'Close of the novel.',
+            description: 'The novel ends with Celie meeting her long lost children, who she believed to be dead..',
             location: {
                 center: [-41.47212410646295, 34.781162343274076], 
                 zoom: 3,
